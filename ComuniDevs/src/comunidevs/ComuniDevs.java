196fdab19b241f8049181c6efb28e3f88053f531
GyanRibeiro <gyanribeiro29@gmail.com>
2023-07-03 14:49:02 -0300

    connecting to the database and add imports 'java.sql.Connection', 'java.sql.DriverManager' and 'java.sql.SQLException'

--- ComuniDevs/src/comunidevs/ComuniDevs.java
+++ ComuniDevs/src/comunidevs/ComuniDevs.java
@@ -8,6 +8,10 @@ package comunidevs;
  * @author gyanr
  */
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ComuniDevs {
     
     /* Método que conecta os drivers */
@@ -22,12 +26,27 @@ public class ComuniDevs {
             System.out.printf("Falha no carregamento! %s", e);
         }
     }
-
+    
+    /*Método que conecta/"cria" o BD*/
+    public static void createBD() {
+        String driver = "jdbc:postgresql://127.0.0.1:5432/DadosDesenvolvedores";
+        try (Connection conn = DriverManager.getConnection(driver, "postgres", "2912")) {
+            if (conn != null) {
+                System.out.println("Connected to the database!");
+            } else {
+                System.out.println("Failed to make connection!");
+        }
+    } catch (SQLException e) {
+        System.err.format("SQL State: %S\n%s", e.getSQLState(), e.getMessage());
+    }
+    }
+    
     public static void main(String[] args) {
         
         
         System.out.println("\n==============================================");
         ComuniDevs.conectDriver();
+        ComuniDevs.createBD();
     }
     
 }
