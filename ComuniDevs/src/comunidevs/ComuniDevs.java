88903f48e448081187adcf5a1fc8f3b69b02d2d0
GyanRibeiro <gyanribeiro29@gmail.com>
2023-07-03 14:37:52 -0300

    add drivers postgresql

---
+++ ComuniDevs/src/comunidevs/ComuniDevs.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package comunidevs;
+
+/**
+ * @author gyanr
+ */
+
+public class ComuniDevs {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
