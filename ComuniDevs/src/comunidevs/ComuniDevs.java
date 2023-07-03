ebbf8f0d93bff90599fbfa5e50ecad2fd539128a
GyanRibeiro <gyanribeiro29@gmail.com>
2023-07-03 14:42:50 -0300

    connecting the drivers with Postgresql

--- ComuniDevs/src/comunidevs/ComuniDevs.java
+++ ComuniDevs/src/comunidevs/ComuniDevs.java
@@ -9,9 +9,25 @@ package comunidevs;
  */
 
 public class ComuniDevs {
+    
+    /* Método que conecta os drivers */
+    static String driverJDBC = "org.postgresql.Driver";
+    
+    public static void conectDriver() {
+        try {
+            System.out.println("Carregando driver JDBC...");
+            Class.forName(driverJDBC);
+            System.out.println("Driver carregado!");
+        } catch (Exception e) {
+            System.out.printf("Falha no carregamento! %s", e);
+        }
+    }
 
     public static void main(String[] args) {
         
+        
+        System.out.println("\n==============================================");
+        ComuniDevs.conectDriver();
     }
     
 }
